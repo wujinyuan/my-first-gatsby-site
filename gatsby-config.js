1fa3738ba31ddee91cb4f70c0175d3f40ba4f9fd
zachary.wu <jinyuan.wu@snowballtech.com>
2024-12-09 12:37:43 +0800

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
